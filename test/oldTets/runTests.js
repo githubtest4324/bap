cbbd4b2b8137f56c9823d30df0b594cd2eb41866
gliviu <grigoresculiviu@gmail.com>
2014-09-16 08:00:55 +0300

    test refactoring

---
+++ test/oldTets/runTests.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var tests1 = require('./tests1')();
+
+function passed(value){
+	return value?'Passed':'!!!!FAILED!!!!';
+}
+
+function runTests() {
+	var res = null;
+	console.log('Tests1 - filter');
+	res = tests1.test1();
+	console.log('tests1/test1: ' + passed(res));
+	res = tests1.test2();
+	console.log('tests1/test2: ' + passed(res));
+    res = tests1.test3();
+    console.log('tests1/test3: ' + passed(res));
+    res = tests1.test4();
+    console.log('tests1/test4: ' + passed(res));
+    res = tests1.test5();
+    console.log('tests1/test5: ' + passed(res));
+}
+
+
+runTests();
+
+
+
